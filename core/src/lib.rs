05c35bdb3c0eb545682214d690b6473b58717988
whitingyan <1712428442@qq.com>
2026-06-11 12:11:01 +0800

    .

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -20,28 +20,23 @@ pub trait Ast<'a, P: Project> {
 
 pub struct AstImpl<'a, P: Project> {
     pub module: &'a mut Module<P>,
-    pub expr_len: usize,
 }
 
 impl<'a, P: Project> AstImpl<'a, P> {
     pub fn new(module: &'a mut Module<P>) -> Self {
         Self {
             module,
-            expr_len: 0,
         }
     }
     pub fn property(&self, expr: &ExprId, offset: usize) -> NodeIdLocal {
-        NodeIdLocal(expr.0 * P::Ast::PROPERTIES_LEN + offset)
+        NodeIdLocal(expr.0 + offset)
     }
     pub fn add_auto(&mut self) -> ExprId {
         let node = self.module.add_auto();
-        debug_assert_eq!(node.0, self.expr_len * P::Ast::PROPERTIES_LEN);
         for _ in 1..P::Ast::PROPERTIES_LEN {
             self.module.add_auto();
         }
-        let ret = ExprId(self.expr_len);
-        self.expr_len += 1;
-        ret
+        ExprId(node.0)
     }
     pub fn add_entry(&mut self, expr: &ExprId) {
         for i in 0..P::Ast::PROPERTIES_LEN {
