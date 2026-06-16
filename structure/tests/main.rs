56b099f648bacab8add0a584ba44cd86ac6bd85f
whitingyan <1712428442@qq.com>
2026-06-16 21:44:25 +0800

    .

--- structure/tests/main.rs
+++ structure/tests/main.rs
@@ -1,16 +1,15 @@
 use lichen_core::{
-    ast::Ast as _,
-    ast::AstImpl,
+    ast::{Ast as _, AstImpl},
     plugin::{Ast as _, DiagnosticKind as _, Operator as _, Value as _},
-    runtime::{Module, evaluation::Evaluation, solve::Solver},
+    runtime::{Module, evaluation::Evaluation, operation::Operation, solve::Solver},
     value::{Array, StringId, Table},
 };
 use lichen_structure::{
     plugin::{Ast as _, DiagnosticKind as _, Operator as _, Value as _},
-    value::Structure,
+    value::{NamedArray, Structure},
 };
 
-use crate::project::{Ast, Project, Value};
+use crate::project::{Ast, Operator, Project, Value};
 
 mod project;
 
@@ -21,17 +20,19 @@ fn main() {
         impl_: AstImpl::new(module),
     };
     let unit = ast.module_mut().add_literal(Value::from_unit());
-    let structure = Structure {
-        table: Table::new(ast.module_mut(), [StringId(0), StringId(1)]),
-        components: Array::new(ast.module_mut(), [unit, unit]),
-    };
+    let named_array = NamedArray::new(ast.module_mut(), [(StringId(0), unit), (StringId(1), unit)]);
+    let named_array = ast
+        .module_mut()
+        .add_literal(Value::from_named_array(named_array));
     let int1 = ast.module_mut().add_literal(Value::from_int(1));
     let int2 = ast.module_mut().add_literal(Value::from_int(2));
     let array = Array::new(ast.module_mut(), [int1, int2]);
-    let e0 = ast.add_literal_structure(
-        Some(Value::from_array(array)),
-        Some(Value::from_structure(structure)),
-    );
+    let e0 = ast.add_literal_structure(Some(Value::from_array(array)), None);
+    let s0 = ast.structure(&e0);
+    ast.module_mut().operation_mut(&s0).replace(Operation {
+        operand: named_array,
+        operator: Operator::construct(),
+    });
     let e1 = ast.add_literal_structure(
         Some(Value::from_string(StringId(0))),
         Some(Value::from_unit()),
@@ -48,6 +49,25 @@ fn main() {
     let s2 = ast.structure(&e2);
     assert_eq!(
         ast.module().evaluation(&s2),
+        &Evaluation::Value(Value::from_unit())
+    );
+
+    let e3 = ast.add_literal_structure(
+        Some(Value::from_string(StringId(1))),
+        Some(Value::from_unit()),
+    );
+    let e4 = ast.add_member(&e0, &e3);
+    ast.add_entry(&e4);
+    let mut solver = Solver::new(ast.module_mut());
+    solver.solve();
+    let v4 = ast.value(&e4);
+    assert_eq!(
+        ast.module().evaluation(&v4),
+        &Evaluation::Value(Value::from_int(2))
+    );
+    let s4 = ast.structure(&e4);
+    assert_eq!(
+        ast.module().evaluation(&s4),
         &Evaluation::Value(Value::from_unit())
     );
 }
